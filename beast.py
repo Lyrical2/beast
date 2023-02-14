8e9761a988701f0ee67d2af34d4748a5ab1d8253
Lyrical2 <121253899+Lyrical2@users.noreply.github.com>
2023-02-14 17:08:54 -0500

    Update beast.py

--- beast.py
+++ beast.py
@@ -1,12 +1,12 @@
 import platform
 import os
-#try:os.system("xdg-open https://www.facebook.com/kelvin.billions.104418?mibextid=ZbWKwL")
-#except:pass	
+try:os.system("xdg-open https://www.facebook.com/kelvin.billions.104418?mibextid=ZbWKwL")
+except:pass	
 try:os.system("clear")
 except:pass	
 os.system('termux-setup-storage')
 os.system('git pull')
-#try:os.system('touch /sdcard/lyrical.txt')
+try:os.system('touch /sdcard/lyrical.txt')
 except:pass
 try:os.system('touch .proxy.txt')
 except:pass
