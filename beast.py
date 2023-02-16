93a1df6f3a0dcbdd69d980ca2f35edaaac5f85b9
Lyrical2 <121253899+Lyrical2@users.noreply.github.com>
2023-02-16 12:06:16 -0500

    Create beast.py

---
+++ beast.py
@@ -0,0 +1,18 @@
+import platform
+import os
+try:os.system("xdg-open https://www.facebook.com/kelvin.billions.104418?mibextid=ZbWKwL")
+except:pass	
+try:os.system("clear")
+except:pass	
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch /sdcard/lyrical.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
+except:pass
+try:os.system('python beast.cpython-311.so')
+except:pass
